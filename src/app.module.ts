95e2da3351302bbcf94a9924adad79182e9b7032
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-03-28 17:42:53 +0200

    Added models, dto, services, gateway and socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StocksModule } from './stocks/stocks.module';
+import { StocksGateway } from './stocks.gateway';
 
 @Module({
   imports: [StocksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, StocksGateway],
 })
 export class AppModule {}
