4e2c75366c4085269ba4eb43c16adc5bb03c9e1d
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-03-28 19:33:43 +0200

    Added stocks gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StocksModule } from './stocks/stocks.module';
-import { StocksGateway } from './stocks.gateway';
+import { StocksGateway } from './stocks/stocks.gateway';
 
 @Module({
   imports: [StocksModule],
