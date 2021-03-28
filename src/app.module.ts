07f1a13f0b782494f77ad13884b3fb971a774d6c
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-03-28 17:31:56 +0200

    Replaced stock with stocks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StockModule } from './stock/stock.module';
+import { StocksModule } from './stocks/stocks.module';
 
 @Module({
-  imports: [StockModule],
+  imports: [StocksModule],
   controllers: [AppController],
   providers: [AppService],
 })
